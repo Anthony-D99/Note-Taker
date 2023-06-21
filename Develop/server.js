b26b46a3e466cdc558ccfe1871d31d4e5b345228
Anthony-D99 <anthonydisaf@yahoo.com>
2023-06-21 17:52:21 -0400

    added start to packages

--- Develop/server.js
+++ Develop/server.js
@@ -15,10 +15,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+  res.sendFile(path.join(__dirname, './public/index.html'))
 );
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+  res.sendFile(path.join(__dirname, './public/notes.html'))
 );
 
 app.get('/api/notes', (req, res) => {
